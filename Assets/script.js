702a91dac82beaff2a79b1750e226fd0adf3beca
Kendra Kwoka <kendrakwoka@gmail.com>
2020-01-07 16:56:12 -0600

    removed unnecessary semicolons

--- Assets/script.js
+++ Assets/script.js
@@ -20,23 +20,23 @@ function generatePassword() {
   
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) ) {
     passwordLength = prompt("Password length must be between 8 - 128 characters.")
-  }; 
+  }
 
   if (confirm("Would you like to include special characters in your password?\nClick OK for yes, CANCEL for no.")) {
     chosenTypes = chosenTypes.concat(specialChars);
-  };
+  }
 
   if (confirm("Would you like to include numbers in your password?\nClick OK for yes, CANCEL for no.")) {
     chosenTypes = chosenTypes.concat(numbers)
-  };
+  }
 
   if (confirm("Would you like to include lowercase letters in your password?\nClick OK for yes, CANCEL for no.")) {
     chosenTypes = chosenTypes.concat(lowerCase);
-  };
+  }
 
   if (confirm("Would you like to include uppercase letters in your password\nClick OK for yes, CANCEL for no.")) {
     chosenTypes = chosenTypes.concat(upperCase);
-  };
+  }
   
   for (i = 0; i < passwordLength; i++) {
     var x = chosenTypes[Math.floor(Math.random()*(chosenTypes.length))];
@@ -45,14 +45,14 @@ function generatePassword() {
   }
   return finalPassword;
 
-};
+}
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-};
+}
 
 function copyFunction() {
   var copyText = document.getElementById("password");
@@ -60,7 +60,7 @@ function copyFunction() {
   copyText.setSelectionRange(0, 99999)
   document.execCommand("copy");
   alert("Copied the text: " + copyText.value);
-};
+}
 
 input.addEventListener('click', function () {
   textarea.value = '';
